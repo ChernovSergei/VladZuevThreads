0c6f1a8bcc7d7d1b0b531b0854009455a0f07d7d
Sergei Chernov <goldengrail@gmail.com>
2024-12-03 14:20:58 +0500

    Lesson 9 Thread interrupt

--- src/main/java/ThreadStop.java
+++ src/main/java/ThreadStop.java
@@ -5,13 +5,17 @@ public class ThreadStop {
     private static final int DURATION = 1;
     private static final String MSG_RESPONSE = "Response was received.";
     private static final String MSG_STOPPED = "Server is stopped.";
+    private static final String MSG_INTERRUPTED = "Server is interrupted.";
     private static final int WAITING = 5;
 
     public static void main(String[] args) throws InterruptedException {
         final Thread thread = new Thread(() ->  {
             try {
-                while (true) {
+                while (!Thread.currentThread().isInterrupted()) {
                     doRequest();
+                    if (Thread.currentThread().isInterrupted()) {
+                        System.out.println(MSG_INTERRUPTED);
+                    }
                 }
             } catch (final InterruptedException e) {
 
@@ -21,7 +25,7 @@ public class ThreadStop {
 
         final Thread stoppingThread = new Thread(() -> {
             if (isServerStopped()) {
-                thread.stop();
+                thread.interrupt();
                 stopServer();
             }
         });
@@ -31,7 +35,6 @@ public class ThreadStop {
 
     private static void doRequest() throws InterruptedException{
         System.out.println(MSG_SENT);
-        TimeUnit.SECONDS.sleep(DURATION);
         System.out.println(MSG_RESPONSE);
     }
 
