01d5a59d1bde4fb8e5e53ee035cc9be5116dcd47
Sergei Chernov <goldengrail@gmail.com>
2024-12-05 21:50:33 +0500

    Lesson 12 Un cought exception handler for each thread

---
+++ src/main/java/ThreadUncoughtException.java
@@ -0,0 +1,25 @@
+public class ThreadUncoughtException {
+    private static final String MSG_TEMPLATE = "Exception was thrown with message '%s' in thread '%s'.\n";
+
+    public static void main(String[] args) {
+       final Thread.UncaughtExceptionHandler uncaughtExceptionHandler = (thread, exception)
+        -> System.out.printf(MSG_TEMPLATE, exception.getMessage(), thread.getName());
+       final Thread firstThread = new Thread(new Task());
+       final Thread secondThread = new Thread(new Task());
+       firstThread.setUncaughtExceptionHandler(uncaughtExceptionHandler);
+       secondThread.setUncaughtExceptionHandler(uncaughtExceptionHandler);
+
+       firstThread.start();
+       secondThread.start();
+    }
+
+    public static final class Task implements Runnable {
+
+        private static final String EXCEPTION_MSG = "I am exception";
+
+        @Override
+        public void run() {
+            throw new RuntimeException(EXCEPTION_MSG);
+        }
+    }
+}
